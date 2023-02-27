c142ccc19bfec17d799398a85474ae7da3ca8406
“Saad <saad1929@gmail.com>
2023-02-27 17:23:33 +0000

    Test

--- WAD_2023_7C/yumyay/views.py
+++ WAD_2023_7C/yumyay/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
+# Create your views here. Test
 def index(request):
     return HttpRespSonse("yumyay")
