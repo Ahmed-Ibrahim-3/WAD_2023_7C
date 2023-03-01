bb5b7577ba1f012edb2ae02a89e09723e7dba541
jens <jensfla@gmail.com>
2023-03-01 15:11:44 +0000

    recipe form

--- WAD_2023_7C/yumyay/forms.py
+++ WAD_2023_7C/yumyay/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from yumyay.models import UserProfile
+from yumyay.models import Category, Cuisine, Recipe
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
@@ -9,3 +9,18 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password',)
 
+class RecipeForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text='Name: ', required=True)
+    # use class method to set author on form submit?
+    author = None
+    category = forms.ChoiceField(queryset=Category.objects.all(), required=True, help_text='Category: ')
+    cuisine = forms.ChoiceField(queryset=Cuisine.objects.all(), required=True, help_text='Cuisine: ')
+    # ingredients, instructions
+    image = forms.ImageField(help_text='An image related to the recipe: ')
+
+    def setAuthor(self, user):
+        author = user
+    
+    class Meta:
+        model = Recipe
+        fields = ('name', 'category', 'cuisine', 'image')
