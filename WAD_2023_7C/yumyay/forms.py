2716c3988bf665d28cc22b93ac6179cfe4b02665
jens <jensfla@gmail.com>
2023-03-01 14:05:14 +0000

    user form

---
+++ WAD_2023_7C/yumyay/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from yumyay.models import UserProfile
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password',)
+
