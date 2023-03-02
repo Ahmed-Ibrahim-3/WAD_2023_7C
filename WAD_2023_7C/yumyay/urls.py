d4fc305f94a8784ffe20e2a2c7392f0c1219ec4d
Nyx Douglas <2559028d@student.gla.ac.uk>
2023-03-02 16:06:43 +0000

    Added html files and made a basic version of recipe, baking, cooking, home and some base files

--- WAD_2023_7C/yumyay/urls.py
+++ WAD_2023_7C/yumyay/urls.py
@@ -6,9 +6,11 @@ from yumyay import views
 app_name = 'yumyay'
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('account/', views.account, name='account'),
-    # path('account/login/', views.log_in, name='log_in'),
-    # path('cooking/', views.cooking, name='cooking'),
-    # path('baking/', views.baking, name='baking'),
-    # path('add_recipe/', views.add_recipe, name='add_recipe'),
+    path('account/', views.account, name='account'),
+    path('account/login/', views.log_in, name='log_in'),
+    path('cooking/', views.cooking, name='cooking'),
+    path('baking/', views.baking, name='baking'),
+    path('add_recipe/', views.add_recipe, name='add_recipe'),
+    path('cooking/cuisine/recipe/', views.recipe_cooking, name='recipe'),
+    path('baking/cuisine/recipe/', views.recipe_baking, name='recipe'),
 ]
--- WAD_2023_7C/yumyay/views.py
+++ WAD_2023_7C/yumyay/views.py
@@ -5,3 +5,32 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'yumyay/home.html')
+
+
+def log_in(request):
+    return render(request, 'yumyay/log_in.html')
+
+
+def account(request):
+    return render(request, 'yumyay/account.html')
+
+
+def cooking(request):
+    return render(request, 'yumyay/cooking.html')
+
+
+def baking(request):
+    return render(request, 'yumyay/baking.html')
+
+
+def add_recipe(request):
+    return render(request, 'yumyay/add_recipe.html')
+
+
+# Nyx fix recipes stuff it's a mess xoxo
+def recipe_cooking(request):
+    return render(request, 'yumyay/recipe.html')
+
+
+def recipe_baking(request):
+    return render(request, 'yumyay/recipe.html')
