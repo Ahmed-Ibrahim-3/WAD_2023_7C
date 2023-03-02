e1539836d44d3b0ae19c9f73bc27ccc414d4a98a
jens <jensfla@gmail.com>
2023-03-02 12:31:31 +0000

    media file setup

--- WAD_2023_7C/WAD_2023_7C/settings.py
+++ WAD_2023_7C/WAD_2023_7C/settings.py
@@ -17,7 +17,11 @@ from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -29,8 +33,6 @@ SECRET_KEY = '$_(b^-58lvncbqce9)mnd3qq%i5b5&!dh&52g1sli4f-&y@mi6'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Application definition
@@ -68,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
