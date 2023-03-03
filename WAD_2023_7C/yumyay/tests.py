187c43e17c492983632892db4e54d5ccb7ddace7
“Saad <saad1929@gmail.com>
2023-03-03 18:08:27 +0000

    Removed comment & Fixed assertFalse

--- WAD_2023_7C/yumyay/tests.py
+++ WAD_2023_7C/yumyay/tests.py
@@ -3,8 +3,7 @@ from django.urls import reverse
 
 # Create your tests here.
 
-#Commented Tests on Purpose - waiting for migrations of models.
-'''class TestHomePageLoggedOut(TestCase):
+class TestHomePageLoggedOut(TestCase):
 
     def test_successful_deployment(self):
         response = self.client.get(reverse("yumyay:home"))
@@ -17,5 +16,5 @@ from django.urls import reverse
     
     def test_guest_home_2(self):
         response = self.client.get(reverse("yumyay:home"))
-        self.assertFalse(response.status_code, 404)
-        self.assertNotContains(response, "Log out")'''
+        self.assertNotEquals(response.status_code, 404)
+        self.assertNotContains(response, "Log out")
