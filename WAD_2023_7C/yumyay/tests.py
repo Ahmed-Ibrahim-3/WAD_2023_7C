121a2c56780f0b18af84869f87e95254eecbba3d
“Saad <saad1929@gmail.com>
2023-03-03 17:36:52 +0000

    Started Home page tests, read comment

--- WAD_2023_7C/yumyay/tests.py
+++ WAD_2023_7C/yumyay/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+#Commented Tests on Purpose - waiting for migrations of models.
+'''class TestHomePageLoggedOut(TestCase):
+
+    def test_successful_deployment(self):
+        response = self.client.get(reverse("yumyay:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_guest_home(self):
+        response = self.client.get(reverse("yumyay:home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Log in")
+    
+    def test_guest_home_2(self):
+        response = self.client.get(reverse("yumyay:home"))
+        self.assertFalse(response.status_code, 404)
+        self.assertNotContains(response, "Log out")'''
