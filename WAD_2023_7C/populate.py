6e4fa9e7648b14f1f38a2b6ff6a812b904a616b1
Ahmed Ibrahim <2653288i@student.gla.ac.uk>
2023-03-03 15:36:39 +0000

    finished & working populate.py -- may need to delete db.sqlite3 for this to work.

--- WAD_2023_7C/populate.py
+++ WAD_2023_7C/populate.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'tango_with_django_project.settings')
+                      'WAD_2023_7C.settings')
 import django
 
 django.setup()
@@ -22,7 +22,7 @@ def populate():
     ]
     thai = [
         {
-            'name': 'content',
+            'name': 'content2',
             'description': 'content',
             'category': 'cooking',
             'cuisine': 'thai',
@@ -33,7 +33,7 @@ def populate():
     ]
     chinese = [
         {
-            'name': 'content',
+            'name': 'content3',
             'description': 'content',
             'category': 'cooking',
             'cuisine': 'chinese',
@@ -44,7 +44,7 @@ def populate():
     ]
     italian = [
         {
-            'name': 'content',
+            'name': 'content4',
             'description': 'content',
             'category': 'cooking',
             'cuisine': 'italian',
@@ -55,7 +55,7 @@ def populate():
     ]
     mexican = [
         {
-            'name': 'content',
+            'name': 'content5',
             'description': 'content',
             'category': 'cooking',
             'cuisine': 'mexican',
@@ -66,7 +66,7 @@ def populate():
     ]
     greek = [
         {
-            'name': 'content',
+            'name': 'content6',
             'description': 'content',
             'category': 'cooking',
             'cuisine': 'greek',
@@ -77,7 +77,7 @@ def populate():
     ]
     cake = [
         {
-            'name': 'content',
+            'name': 'content7',
             'description': 'content',
             'category': 'baking',
             'cuisine': 'cake',
@@ -88,7 +88,7 @@ def populate():
     ]
     brownie = [
         {
-            'name': 'content',
+            'name': 'content8',
             'description': 'content',
             'category': 'baking',
             'cuisine': 'brownie',
@@ -99,7 +99,7 @@ def populate():
     ]
     bread = [
         {
-            'name': 'bread',
+            'name': 'content9',
             'description': 'content',
             'category': 'baking',
             'cuisine': 'bread',
@@ -110,7 +110,7 @@ def populate():
     ]
     pastries = [
         {
-            'name': 'content',
+            'name': 'content10',
             'description': 'content',
             'category': 'baking',
             'cuisine': 'pastries',
@@ -121,7 +121,7 @@ def populate():
     ]
     cupcakes = [
         {
-            'name': 'content',
+            'name': 'content11',
             'description': 'content',
             'category': 'baking',
             'cuisine': 'cupcakes',
@@ -132,7 +132,7 @@ def populate():
     ]
     cookies = [
         {
-            'name': 'content',
+            'name': 'content12',
             'description': 'content',
             'category': 'baking',
             'cuisine': 'cookies',
@@ -143,17 +143,49 @@ def populate():
     ]
 
     cuisines = {
-        'Indian': indian,
-        'Thai': thai,
-        'Chinese': chinese,
-        'Italian': italian,
-        'Mexican': mexican,
-        'Greek': greek,
-        'Cake': cake,
-        'Brownies': brownie,
-        'Bread': bread,
-        'Pastries': pastries,
-        'Cupcakes': cupcakes,
-        'Cookies': cookies}
+        'Indian': {'name': 'indian', 'recipes': indian},
+        'Thai': {'name': 'Thai', 'recipes': thai},
+        'Chinese': {'name': 'Chinese', 'recipes': chinese},
+        'Italian': {'name': 'Italian', 'recipes': italian},
+        'Mexican': {'name': 'Mexican', 'recipes': mexican},
+        'Greek': {'name': 'Greek', 'recipes': greek},
+        'Cake': {'name': 'Cake', 'recipes': cake},
+        'Brownies': {'name': 'Brownies', 'recipes': brownie},
+        'Bread': {'name': 'Bread', 'recipes': bread},
+        'Pastries': {'name': 'Pastries', 'recipes': pastries},
+        'Cupcakes': {'name': 'Cupcakes', 'recipes': cupcakes},
+        'Cookies': {'name': 'Cookies', 'recipes': cookies}
+    }
 
-    # write brains of the code
+    def add_cuisine(name):
+        print("weeeeeeee")
+        cuis = Cuisine.objects.get_or_create(name=name)[0]
+        cuis.save()
+        return name
+
+    def add_recipe(name, description, cuis, ingredients, instructions):
+        recp = Recipe.objects.get_or_create(name=name, description=description,
+                                            cuisine=cuis, ingredients=ingredients, instructions=instructions)[0]
+        print("wooooooo")
+        recp.name = name
+        print(name)
+        recp.description = description
+        recp.cuisine = cuis
+        recp.ingredients = ingredients
+        recp.instructions = instructions
+        recp.save()
+        return recp
+
+    for cuisine, cuisine_data in cuisines.items():
+        c = add_cuisine(cuisine_data['name'])
+        for r in cuisine_data['recipes']:
+            add_recipe(r['name'], r['description'], c, r['ingredients'], r['instructions'])
+
+    for c in Cuisine.objects.all():
+        for r in Recipe.objects.filter(cuisine=c):
+            print(f'- {c}:{r}')
+
+
+if __name__ == '__main__':
+    print('starting populate script')
+    populate()
--- WAD_2023_7C/yumyay/models.py
+++ WAD_2023_7C/yumyay/models.py
@@ -5,9 +5,9 @@ from django.db import models
 # Create your models here.
 
 class Cuisine(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=64, unique=True)
     img = models.ImageField()
-    slug = models.SlugField(unique=True)
+    # slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
