a67cd8e367a9f2dc80dc72a8452e7075b9cb3d99
jens <jensfla@gmail.com>
2023-03-12 18:04:23 +0000

    button animation

--- WAD_2023_7C/static/javascript/browse.js
+++ WAD_2023_7C/static/javascript/browse.js
@@ -8,11 +8,50 @@ next.addEventListener("click", () => change(1));
 prev.addEventListener("click", () => change(-1));
 
 function change(change){
+    let animationTime = 300;
+
+    const animationFadeOut = [
+        {
+            transform:"scaleX(1)",
+        },
+        {
+            transform:"scaleX(0)",
+        }
+    ];
+    const animationFadeIn = [
+        {
+            transform:"scaleX(0)",
+        },
+        {
+            transform:"scaleX(1)",
+        }
+    ];
+    const control = {
+        duration:animationTime,
+        fill:'forwards',
+    }
+
+    if(change > 0){
+        recipes[i].style.transformOrigin = "left"
+    } else {
+        recipes[i].style.transformOrigin = "right"
+    }
+    
     recipes[i].classList.remove('active')
     recipes[i].classList.add('inactive')
-
+    recipes[i].animate(animationFadeOut,control)
+    
+    
     i = (i+change+recipes.length) % recipes.length
 
     recipes[i].classList.add('active')
     recipes[i].classList.remove('inactive')
+
+    if(change <= 0){
+        recipes[i].style.transformOrigin = "left"
+    } else {
+        recipes[i].style.transformOrigin = "right"
+    }
+
+    recipes[i].animate(animationFadeIn, control)
 }
