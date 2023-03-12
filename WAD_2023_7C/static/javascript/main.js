ecbac373e903056bef5cbb44fde560d4abc55944
Saad1929 <saad2baig123@gmail.com>
2023-03-12 16:13:01 +0000

    Cooking/Baking Buttons JavaScript

--- WAD_2023_7C/static/javascript/main.js
+++ WAD_2023_7C/static/javascript/main.js
@@ -1,2 +1,20 @@
 // Main JavaScript file
 console.log('Main loaded')
+
+// Buttons Cooking/Baking
+const cbButton = document.querySelectorAll(".btn-rounded");
+
+cbButton.forEach(cButton => {
+    cButton.addEventListener("click", () => {
+        var scaleFactor = 0.8;
+        var transitionDuration = "0.1s";
+
+        cButton.style.transform = `scale(${scaleFactor})`;
+        cButton.style.transition = `transform ${transitionDuration}`;
+        setTimeout(() => {
+            cButton.style.transform = "";
+            cButton.style.transition = "";
+          }, 110);
+    })
+})
+
