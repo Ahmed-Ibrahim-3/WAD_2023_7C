42ba638f0c14d0d60cef9f4053e1bf3f6143c237
jens <jensfla@gmail.com>
2023-03-14 00:03:37 +0000

    form options

---
+++ WAD_2023_7C/static/javascript/forms.js
@@ -0,0 +1,21 @@
+const options = {
+    'cooking': ["Chinese", "Greek", "Indian", "Italian", "Mexican", "Thai"],
+    'baking': ["Bread", "Brownies", "Cookies", "Cake", "Cupcake", "Patries"],
+}  
+
+const categorySelect = document.querySelector("#categorySelect");
+const cuisineSelect = document.querySelector("#cuisineSelect");
+
+addOptions('cooking')
+
+categorySelect.addEventListener("change", () => {
+    addOptions(categorySelect.value);
+})  
+ 
+function addOptions(category){
+    cuisineSelect.innerHTML = "";
+    let formOptions = options[category];
+    for(i = 0; i < formOptions.length; i++){
+        cuisineSelect.innerHTML += `<option value="${formOptions[i].toLowerCase()}">${formOptions[i]}</option>`
+    }
+}
