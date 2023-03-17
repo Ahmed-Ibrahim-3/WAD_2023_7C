f3caee751313903bf958483f6cb2ee7bbe9fbed4
jens <jensfla@gmail.com>
2023-03-17 10:30:29 +0000

    form fix

--- WAD_2023_7C/yumyay/forms.py
+++ WAD_2023_7C/yumyay/forms.py
@@ -32,10 +32,9 @@ class RecipeForm(forms.ModelForm):
         exclude = ('likes','author')
 
 class EditDetailsForm(forms.ModelForm):
-    username = forms.CharField(required=True)
     email = forms.EmailField(required=True)
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email')
